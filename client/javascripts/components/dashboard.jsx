61e1def2deb0e776596e2af8556e5405cac1ee52
Akash Trivedi <akash@citizencode.io>
2016-11-30 16:01:00 -0800

    Set up basic framework for frontend.

--- client/javascripts/components/dashboard.jsx
+++ client/javascripts/components/dashboard.jsx
@@ -1,66 +1,21 @@
 const React = require('react')
 const { connect } = require('react-redux')
 
-const { selectCar, fetchCarData } = require('../actions/dashboard')
-const EngineStatus = require('./engine')
+const { fetchProductData } = require('../actions/dashboard')
 
 const Dashboard = React.createClass({
   render() {
     return (
       <div>
-        <div className="select_button_wrapper">
-          <div className="styled-select">
-            <select onChange={ (selected) => this.props.selectCar(selected) }>
-              <option value="1234">1234</option>
-              <option value="1235">1235</option>
-            </select>
-          </div>
+        <div className="fetch_products_button_container">
+          <button
+            className="button_fetch_products"
+            onClick={ () => {
+              this.props.fetchProductData() } }>
+             Reset Product Page
+          </button>
+        </div >
 
-          <div className="select_vehicle_button_container">
-            <button
-              className="button_select_vehicle"
-              onClick={ () => {
-                this.props.fetchCarData(this.props.carId) } }>
-              Select Vehicle
-            </button>
-          </div >
-        </div>
-
-        <table>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Data</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td>Vehicle Info: </td>
-              <td>{ this.props.carInformation.get('vehicleInfo') }</td>
-            </tr>
-
-            <tr>
-              <td>Security: </td>
-              <td>{ this.props.carInformation.get('securityInfo') }</td>
-            </tr>
-
-            <tr>
-              <td>Fuel Range: </td>
-              <td>{ this.props.carInformation.get('fuelRange') }</td>
-            </tr>
-
-            <tr>
-              <td>Battery Range: </td>
-              <td>{ this.props.carInformation.get('batteryRange') }</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <EngineStatus
-          carId={ this.props.carId }
-          displayInfo={ this.props.displayEngineInfo }
-        />
       </div>
     )
   }
@@ -68,16 +23,14 @@ const Dashboard = React.createClass({
 
 function mapStateToProps(state) {
   return {
-    carId: state.dashboard.get('carId'),
-    carInformation: state.dashboard.get('carInformation'),
-    displayEngineInfo: state.dashboard.get('displayEngineInfo')
+    //placeholder
+    productInfo: state.productInfo.get('productPictures'),
   }
 }
 
 function mapDispatchToActions(dispatch) {
   return {
-    selectCar: (...args) => selectCar(dispatch, ...args),
-    fetchCarData: (...args) => fetchCarData(dispatch, ...args)
+    fetchProductData: (...args) => fetchCarData(dispatch, ...args)
   }
 }
 
